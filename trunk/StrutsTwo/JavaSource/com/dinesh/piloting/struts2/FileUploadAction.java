d9f5623d5d55c3c6a1d0c4cbf3ff5a7508c7597d
dinesh13284@gmail.com <dinesh13284@gmail.com@03aa0ab5-ddc1-8c6f-32b6-8f087ffdf6b7>
2012-11-05 12:37:30 +0000

    file upload example added

---
+++ trunk/StrutsTwo/JavaSource/com/dinesh/piloting/struts2/FileUploadAction.java
@@ -0,0 +1,92 @@
+package com.dinesh.piloting.struts2;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class FileUploadAction extends ActionSupport implements ServletRequestAware{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1349574007971286035L;
+	private HttpServletRequest servletRequest;
+	private File userImage;
+    private String userImageContentType;
+    private String userImageFileName;
+	
+    public String execute(){
+    	try{
+            String filePath = servletRequest.getSession().getServletContext().getRealPath("/");
+            System.out.println("Server Path : "+ filePath);
+            File fileToCreate = new File(filePath,this.userImageFileName);
+            FileUtils.copyFile(this.userImage,fileToCreate);
+    	}catch(Exception ex){
+    		ex.printStackTrace();
+    		addActionError(ex.getMessage());
+    		return INPUT;
+    	}
+    	return SUCCESS;
+    }
+    
+	/**
+	 * @return the servletRequest
+	 */
+	public HttpServletRequest getServletRequest() {
+		return servletRequest;
+	}
+
+	/**
+	 * @return the userImage
+	 */
+	public File getUserImage() {
+		return userImage;
+	}
+
+	/**
+	 * @param userImage the userImage to set
+	 */
+	public void setUserImage(File userImage) {
+		this.userImage = userImage;
+	}
+
+	/**
+	 * @return the userImageContentType
+	 */
+	public String getUserImageContentType() {
+		return userImageContentType;
+	}
+
+	/**
+	 * @param userImageContentType the userImageContentType to set
+	 */
+	public void setUserImageContentType(String userImageContentType) {
+		this.userImageContentType = userImageContentType;
+	}
+
+	/**
+	 * @return the userImageFileName
+	 */
+	public String getUserImageFileName() {
+		return userImageFileName;
+	}
+
+	/**
+	 * @param userImageFileName the userImageFileName to set
+	 */
+	public void setUserImageFileName(String userImageFileName) {
+		this.userImageFileName = userImageFileName;
+	}
+
+	@Override
+	public void setServletRequest(HttpServletRequest servletRequest) {
+		this.servletRequest = servletRequest;
+	}
+	
+	
+}
