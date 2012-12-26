d0deef14f954905c8a14c35186b3568e9e2f41a4
dinesh13284@gmail.com <dinesh13284@gmail.com@03aa0ab5-ddc1-8c6f-32b6-8f087ffdf6b7>
2012-12-26 08:13:27 +0000

    test

---
+++ trunk/backup/JavaSource/com/dinesh/piloting/action/SearchAction.java
@@ -0,0 +1,40 @@
+package com.dinesh.piloting.action;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import com.dinesh.piloting.form.SearchForm;
+import com.dinesh.piloting.obj.Employee;
+import com.dinesh.piloting.service.SearchService;
+
+public class SearchAction extends Action {
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		SearchService service = new SearchService();
+		ArrayList<Employee> results;
+
+		SearchForm searchForm = (SearchForm) form;
+		// Perform employee search based on what criteria was entered.
+
+		String name = searchForm.getName();
+		if (name != null && name.trim().length() > 0) {
+			results = service.searchByName(name);
+		} else {
+			results = service.searchBySsNum(searchForm.getSsNum().trim());
+		}
+		// Place search results in SearchForm for access by JSP.
+		searchForm.setResults(results);
+		// Forward control to this Action's input page.
+		return mapping.getInputForward();
+	}
+
+}
